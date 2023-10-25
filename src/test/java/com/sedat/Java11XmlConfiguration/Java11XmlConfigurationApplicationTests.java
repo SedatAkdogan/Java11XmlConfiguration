568adf1e7481e88cac958ece38fe9fd09309e96c
Sedat <sb.akdgn@gmail.com>
2023-10-25 09:48:06 +0300

    Initial commit

---
+++ src/test/java/com/sedat/Java11XmlConfiguration/Java11XmlConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedat.Java11XmlConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11XmlConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
